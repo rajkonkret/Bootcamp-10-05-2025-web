a7a058366eac81e7955972375d2c7759ea132b31
radoslawjaniak <rajkokret660@gmail.com>
2025-08-02 15:32:04 +0200

    [15 24] add route /login, fix client_id

--- web2/zad4/main.py
+++ web2/zad4/main.py
@@ -15,12 +15,13 @@ app = FastAPI()
 JWT_SECRET = os.getenv("JWT_SECRET")
 JWT_ALGO = "H256"
 
-CLIENT_ID = os.getenv("JWT_SECRET")
+CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
 BACK_URI = os.getenv("BACK_URI")
 
 print(BACK_URI)
+
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -33,3 +34,19 @@ def home(request: Request):
     </body>
     </html>
     """
+
+
+@app.get("/login")
+def login():
+    google_auth_url = (
+        "https://accounts.google.com/o/oauth2/v2/auth"
+        "?response_type=code"
+        f"&client_id={CLIENT_ID}"
+        f"&redirect_uri={REDIRECT_URI}"
+        "&scope=openid%20email%20profile"
+        "&access_type=offline"
+    )
+    print(google_auth_url)
+    return RedirectResponse(google_auth_url)
+
+

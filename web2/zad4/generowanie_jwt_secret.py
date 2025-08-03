a54b6de5f024108e3ad2b05bdced84f64d3f92e2
radoslawjaniak <rajkokret660@gmail.com>
2025-08-03 09:38:25 +0200

    [16 3] add generowanie_jwt_secret.py

---
+++ web2/zad4/generowanie_jwt_secret.py
@@ -0,0 +1,9 @@
+import secrets
+
+print(secrets.token_urlsafe(32))
+print(secrets.token_urlsafe(48)) # ten dla HS256
+print(secrets.token_urlsafe(64))
+
+# 0ctVcYFGnzUjxVhf6wv1k7Dx-V05Yw0qkawUvhnd7Gw
+# 8MvyDpgsZur84ZVEN3gIxKK_ChSQXBFSDNu115TEnH7DgZibrWuuE5e32IYkOKHU
+# j3BwtbpHTtggwBdyCgzAp7YQZ9BKt-_FII7mnVLyTDLZ54Y5sMCOqvQphEaNZW6AyoSMXAwuOoioPNUQYP6Yjw

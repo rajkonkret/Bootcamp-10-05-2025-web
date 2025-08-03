cab671dc9b41f0ee7946b95ca26bf5d91066624d
radoslawjaniak <rajkokret660@gmail.com>
2025-08-03 14:08:32 +0200

    [16 15] add class Cantor and CantorOffer

--- web4_with_templates/app.py
+++ web4_with_templates/app.py
@@ -3,6 +3,38 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 
+class Currency:
+    def __init__(self, code, name, flag):
+        self.code = code
+        self.name = name
+        self.flag = flag
+
+    def __repr__(self):
+        return f'<Currency {self.code}>'
+
+
+class CantorOffer:
+
+    def __init__(self):
+        self.currencies = []
+
+    def load_offer(self):
+        """
+        Ładuje dane do sytemu
+        :return:
+        """
+        self.currencies.append(Currency('USD', "Dollar", 'currencies/dollar.png'))
+        self.currencies.append(Currency('EUR', "Euro", 'currencies/euro.jpg'))
+        self.currencies.append(Currency('HUF', "Forint", 'currencies/huf.jpg'))
+        self.currencies.append(Currency('PLN', "Zloty", 'currencies/zloty.jpg'))
+
+    def get_by_code(self, code):
+        for currency in self.currencies:
+            if currency.code == code:
+                return currency
+        return Currency('unknown', 'unknown', 'kantor.png')
+
+
 @app.route("/")
 def index():
     return "This is index"

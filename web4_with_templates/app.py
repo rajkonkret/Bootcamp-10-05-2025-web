03e440f8624e18492de90eb2157569bf7a60fedc
radoslawjaniak <rajkokret660@gmail.com>
2025-08-03 12:35:18 +0200

    [16 11] add exchange_results.html

--- web4_with_templates/app.py
+++ web4_with_templates/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -7,6 +7,12 @@ app = Flask(__name__)
 def index():
     return "This is index"
 
+# http://127.0.0.1:5000/exchange
+# jinja2 - silnik uzywany w renderowaniu templatek z kodu pythonowego
+# 127.0.0.1 - - [03/Aug/2025 12:32:47] "POST /exchange HTTP/1.1" 200 -
+@app.route("/exchange", methods=['GET', 'POST'])
+def exchange():
+     return render_template('exchange.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)

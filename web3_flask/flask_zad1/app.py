dd84261dcd3592eee6e5d226593a7cfe2300cf63
radoslawjaniak <rajkokret660@gmail.com>
2025-08-03 10:13:40 +0200

    [16 5] update web3_flask

--- web3_flask/flask_zad1/app.py
+++ web3_flask/flask_zad1/app.py
@@ -4,9 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# http://127.0.0.1:5000 - 5000 standartowy port dla Flaska
 @app.route("/")
 def index():
     return "Hello World!!!"
 
+# 127.0.0.1:5000/about
+@app.route("/about")
+def about():
+    a = 10
+    b = 1
+    # return "<h1>We are programers</h1>"
+    return f"<h1>We are programers {a / b}</h1>"
+
+# http://127.0.0.1:5000/error
+@app.route("/error")
+def error():
+    a = 10
+    b = 0
+    return f"<h1>We are programers {a / b}</h1>"
+
+# http://127.0.0.1:5000/cantor/usd/100
+# @app.route("/cantor/<currency>/<amount>")
+# gdy dodamy int, flask nie widzi url dla takiej operacji:
+# 127.0.0.1 - - [03/Aug/2025 10:07:53] "GET /cantor/eur/sto HTTP/1.1" 404 -
+@app.route("/cantor/<string:currency>/<int:amount>")
+def cantor(currency, amount): # nazwy zmiennych odpowiadaja nazwą parametrów w url
+    message = f"<h1>You selected {currency} and {amount}</h1>"
+    return message
+
 if __name__ == '__main__':
     app.run(debug=True)
+# port standardowy 5000

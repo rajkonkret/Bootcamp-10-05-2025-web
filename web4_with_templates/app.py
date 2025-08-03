3a9ce270a419fdb00931ed6c0dbc64fbd9e792a1
radoslawjaniak <rajkokret660@gmail.com>
2025-08-03 14:18:24 +0200

    [16 16] add for in exchnahe.html

--- web4_with_templates/app.py
+++ web4_with_templates/app.py
@@ -1,3 +1,5 @@
+from http.cookiejar import offset_from_tz_string
+
 from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
@@ -29,6 +31,11 @@ class CantorOffer:
         self.currencies.append(Currency('PLN', "Zloty", 'currencies/zloty.jpg'))
 
     def get_by_code(self, code):
+        """
+        Zwraca obiekt klasy Currency na podstawie kodu waluty
+        :param code: kod waluty, którą chcemy wyszukać
+        :return:
+        """
         for currency in self.currencies:
             if currency.code == code:
                 return currency
@@ -45,8 +52,12 @@ def index():
 # 127.0.0.1 - - [03/Aug/2025 12:32:47] "POST /exchange HTTP/1.1" 200 -
 @app.route("/exchange", methods=['GET', 'POST'])
 def exchange():
+
+    offer = CantorOffer()
+    offer.load_offer()
+
     if request.method == "GET":
-        return render_template('exchange.html')
+        return render_template('exchange.html' , offer=offer)
     else:
         currency = "EUR"
         if "currency" in request.form:

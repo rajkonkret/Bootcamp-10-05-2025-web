c48ee06fd52d3ac3bbdd6aaa0600d65822560d91
radoslawjaniak <rajkokret660@gmail.com>
2025-08-16 15:45:58 +0200

    [17 33] add baza_users.py

---
+++ web5_bootstrap/baza_users.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+try:
+    conn = sqlite3.connect("data/cantor.db")
+    c = conn.cursor()
+
+    # query = """
+    # CREATE TABLE IF NOT EXISTS transactions (
+    # id INTEGER PRIMARY KEY,
+    # currency TEXT,
+    # amount INTEGER,
+    # user TEXT);
+    # """
+    #
+    # c.execute(query)
+    # conn.commit()
+
+    create_users = """CREATE TABLE IF NOT EXISTS users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password TEXT,
+    is_active BOOLEAN NOT NULL DEFAULT 0,
+    is_admin BOOLEAN NOT NULL DEFAULT 0);
+    """
+    c.execute(create_users)
+    conn.commit()
+
+except sqlite3.Error as e:
+    print("Błąd:", e)
+finally:
+    if conn:
+        conn.close()

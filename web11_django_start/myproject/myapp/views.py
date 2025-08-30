38a611ff7d173b6e5c3b9a1ac8041251bc2b9263
radoslawjaniak <rajkokret660@gmail.com>
2025-08-30 15:33:40 +0200

    [19 24] add html

--- web11_django_start/myproject/myapp/views.py
+++ web11_django_start/myproject/myapp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
+# def home(request):
+#     return HttpResponse("Witaj w mojej aplikacji Django")
+
 def home(request):
-    return HttpResponse("Witaj w mojej aplikacji Django")
+    return render(request, 'myapp/index.html', {"message": "Django działa!"})

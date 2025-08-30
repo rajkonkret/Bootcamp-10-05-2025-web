381a1d1b7478683e2f9cf71d707859198e21625a
radoslawjaniak <rajkokret660@gmail.com>
2025-08-30 14:16:46 +0200

    [19 20] first view

--- web11_django_start/myproject/myapp/urls.py
+++ web11_django_start/myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+# urls.py w aplikacji
 from .views import home
 
 urlpatterns = [
-    path('', home, name="home") # endpoint '/'
+    path('', home, name="home")  # endpoint '/'
 ]
